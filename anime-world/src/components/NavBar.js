223750d5a6d0c20e40d2e98b2bbd02d67df359d9
Aurian Bastos <aurianacimento0@gmail.com>
2025-11-22 12:10:58 -0300

    Cria o componente de NavBar do projeto
    cria o componente de Navbar para utilizar nas paginas

---
+++ anime-world/src/components/NavBar.js
@@ -0,0 +1,45 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function NavBar(){
+
+    return(
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a href="#" className="navbar-brand mx-4">AnimeWorld</a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+                <span class="navbar-toggler-icon"></span>
+
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+            <ul class="navbar-nav ms-auto">
+                    
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Animes</a>
+                    </li>
+
+                    
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Animações</a>
+                    </li>
+
+                    
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Galeria</a>
+                    </li>
+
+                    
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Catalogação</a>
+                    </li>
+
+                </ul>
+
+            </div>
+        </nav> 
+    )
+}
+
+export default NavBar;
